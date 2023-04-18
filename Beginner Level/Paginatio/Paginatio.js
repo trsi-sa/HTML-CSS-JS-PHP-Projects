a9ae701882e5bdaf68b4696d6f19edfb5fd8de7a
TLER AL-BISHI <111729973+tlersa@users.noreply.github.com>
2023-04-18 05:14:58 +0300

    Add files via upload

---
+++ Beginner Level/Paginatio/Paginatio.js
@@ -0,0 +1,68 @@
+const startBtn = document.querySelector("#startBtn"),
+endBtn = document.querySelector("#endBtn"),
+prevNext = document.querySelectorAll(".prevNext"),
+numbers = document.querySelectorAll(".link"),
+modeSwitch = document.querySelector(".mode-switch"),
+body = document.querySelector("body");
+
+let currentStep = 0;
+
+const updateBtn = () => {
+    if (currentStep === 4) {
+        endBtn.disabled = true;
+        prevNext[1].disabled = true; }
+    else if (currentStep === 0) {
+        startBtn.disabled = true;
+        prevNext[0].disabled = true; }
+    else {
+        endBtn.disabled = false;
+        prevNext[1].disabled = false;
+        startBtn.disabled = false;
+        prevNext[0].disabled = false; } };
+
+numbers.forEach((number, numIndex) => {
+    number.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        currentStep = numIndex;
+
+        document.querySelector(".active").classList.remove("active");
+        
+        number.classList.add("active");
+        updateBtn(); }); });
+
+prevNext.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        currentStep += e.target.id === "next" ? 1 : -1;
+        numbers.forEach((number, numIndex) => {
+        number.classList.toggle("active", numIndex === currentStep);
+        updateBtn(); }); }); });
+
+startBtn.addEventListener("click", () => {
+    document.querySelector(".active").classList.remove("active");
+    numbers[0].classList.add("active");
+    currentStep = 0;
+    updateBtn();
+    endBtn.disabled = false;
+    prevNext[1].disabled = false; });
+
+endBtn.addEventListener("click", () => {
+    document.querySelector(".active").classList.remove("active");
+  
+    numbers[4].classList.add("active");
+    currentStep = 4;
+    updateBtn();
+    startBtn.disabled = false;
+    prevNext[0].disabled = false; });
+
+if (localStorage.getItem("mode") === "الوضع المظلم") {
+    body.classList.add("dark");
+    modeSwitch.textContent = "الوضع الفاتح"; }
+        
+modeSwitch.addEventListener("click", () => {
+    body.classList.toggle("dark");
+    
+    const isDarkMode = body.classList.contains("dark");
+            
+    modeSwitch.textContent = isDarkMode ? "الوضع الفاتح" : "الوضع المظلم";
+    localStorage.setItem("mode", isDarkMode ? "الوضع المظلم" : "الوضع الفاتح"); });
