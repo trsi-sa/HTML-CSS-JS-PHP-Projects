2993766dcb9a142c3472b0198b6ec69dbac16176
TLER AL-BISHI <111729973+tlersa@users.noreply.github.com>
2023-05-13 23:35:49 +0300

    Update Clock.js

--- Pro Level/Clock/Clock.js
+++ Pro Level/Clock/Clock.js
@@ -1,20 +1,7 @@
 const body = document.querySelector("body"),
 hourHand = document.querySelector(".hour"),
 minuteHand = document.querySelector(".minute"),
-secondHand = document.querySelector(".second"),
-modeSwitch = document.querySelector(".mode-switch");
-
-if (localStorage.getItem("mode") === "الوضع المظلم") {
-    body.classList.add("dark");
-    modeSwitch.textContent = "الوضع الفاتح"; }
-
-modeSwitch.addEventListener("click", () => {
-    body.classList.toggle("dark");
-
-    const isDarkMode = body.classList.contains("dark");
-    
-    modeSwitch.textContent = isDarkMode ? "الوضع الفاتح" : "الوضع المظلم";
-    localStorage.setItem("mode", isDarkMode ? "الوضع المظلم" : "الوضع الفاتح"); });
+secondHand = document.querySelector(".second");
 
 const updateTime = () => {
     let date = new Date(),
